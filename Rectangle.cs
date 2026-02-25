5060b1248dcec969cf61c4627cfc8d8896a2c52b
Sitkov <pozetive.je@gmail.com>
2026-02-25 15:17:00 +0500

    first upload

---
+++ Rectangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryShape
+{
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public Rectangle (double w, double h)
+        {
+            if (w <= 0 || h <= 0)
+            {
+                throw new ArgumentException("Стороны не могут быть меньше или равны нулю");
+            }
+            Width = w;
+            Height = h;
+        }
+        public override double Area ()
+        {
+            return Width * Height;
+        }
+
+        public override double Perimeter ()
+        {
+            return (Width + Height) * 2;
+        }
+
+        public override string ToString ()
+        {
+            return $"Информация прямоугольника: \n" +
+                   $"Сторона а - {Width} \n" +
+                   $"Сторона b - {Height} \n" +
+                   $"Площадь - {Area()} \n" +
+                   $"Периметр - {Perimeter()} \n";
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryShape
+{
+    public class Triangle : Shape
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+        public Triangle (double a, double b, double c )
+        {
+            if (a < 0 || b < 0 || c < 0 )
+            {
+                throw new ArgumentException("Стороны не могут быть меньше или равны нулю");
+            }
+            SideA = a;
+            SideB = b; 
+            SideC = c;
+        }
+
+        public override double Area ()
+        {
+            double a = Perimeter() / 2;
+            return Math.Sqrt(a*(a-SideA)*(a - SideB)*(a - SideC));
+        }
+
+        public override double Perimeter ()
+        {
+            return SideA + SideB + SideC;
+        }
+
+        public override string ToString ()
+        {
+            return $"Информация прямоугольника: \n" +
+                   $"Сторона а - {SideA} \n" +
+                   $"Сторона b - {SideB} \n" +
+                   $"Сторона b - {SideC} \n" +
+                   $"Площадь - {Area()} \n" +
+                   $"Периметр - {Perimeter()} \n";
+        }
+    }
+}
