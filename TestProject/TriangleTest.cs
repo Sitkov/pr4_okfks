c1aadb9533f94693e428d881edaf664c6cda21d1
Sveta616 <svetamarinina44@gmail.com>
2026-03-06 17:31:46 +0500

    add tests

---
+++ TestProject/TriangleTest.cs
@@ -0,0 +1,14 @@
+﻿using GeometryShape;
+
+namespace TestProject
+{
+    public class TriangleTest
+    {
+        [Fact]
+        public void RectangleCorrectPerimeter ()
+        {
+            Rectangle rectangle = new Rectangle(-4, -5);
+            Assert.Equal(18, rectangle.Perimeter());
+        }
+    }
+}
--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5060b1248dcec969cf61c4627cfc8d8896a2c52b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a404c78e1564734f0f2291b5218ae9421215c2bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
